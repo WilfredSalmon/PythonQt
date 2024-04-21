8f796bd2dd3888adcf96be64a12793bb38258203
WilfredSalmon <was29@cam.ac.uk>
2024-04-21 20:05:28 +0100

    Fixing the size of the window

--- first_application.py
+++ first_application.py
@@ -1,4 +1,5 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtCore import QSize
 
 class Main_Window(QMainWindow):
     def __init__(self):
@@ -7,8 +8,9 @@ class Main_Window(QMainWindow):
         self.setWindowTitle("Example Application")
         
         button = QPushButton("Press Here.")
-
         self.setCentralWidget(button)
+
+        self.setFixedSize(QSize(500,200))
 
 app = QApplication([])
 
