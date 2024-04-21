41ae7862b2831074a4b9293c6195b5fb64dfe488
WilfredSalmon <was29@cam.ac.uk>
2024-04-21 20:04:06 +0100

    Example Custom Window by inheriting

--- first_application.py
+++ first_application.py
@@ -1,8 +1,18 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class Main_Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Example Application")
+        
+        button = QPushButton("Press Here.")
+
+        self.setCentralWidget(button)
 
 app = QApplication([])
 
-window = QMainWindow()
+window = Main_Window()
 window.show()
 
 app.exec() # Starts the event loop
