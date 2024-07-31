eb98de9404f6e38403aa5436672c6f8feb72c8b5
WilfredSalmon <was29@cam.ac.uk>
2024-07-31 17:35:56 +0100

    multiple signals

--- second_application.py
+++ second_application.py
@@ -1,31 +1,48 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtCore import QSize
+
+window_titles = [
+    "Ouch",
+    "Oi",
+    "That hurt",
+    "Stop poking me!",
+    "Are you even listening to me?!",
+    "That's very rude you know!",
+    "I'm warning you",
+    "I will break those buttons...",
+    "Final chance",
+    "No more buttons for you"
+]
 
 class Main_Window(QMainWindow):
     
     def __init__(self):
         super().__init__()
         
-        self.setWindowTitle("A different window!")
+        self.setWindowTitle("Hello there!")
+        self.setMinimumSize(QSize(1000,400))
         
-        self.button = QPushButton("Press Me!")
-        self.button.setCheckable(True)
+        self.button = QPushButton("Poke")
+        self.button_2 = QPushButton("Prod")
+
+        self.presses = 0
         self.button.clicked.connect(self.button_pressed)
-        self.button.clicked.connect(self.print_button_state)
-        self.button.released.connect(self.button_released)
-
+        self.button_2.clicked.connect(self.button_pressed)
+        self.windowTitleChanged.connect(self.window_title_changed)
+        
         self.setCentralWidget(self.button)
-
-    def button_released(self):
-        print("button released, now", "pressed" if self.button.isChecked() else "not pressed")
+        self.setMenuWidget(self.button_2)
 
     def button_pressed(self):
-        print("button pressed!")
-        self.button.setEnabled(False)
-        self.button.setText("Pressed!")
-        self.setWindowTitle("Now this app is boring")
+        self.setWindowTitle(window_titles[self.presses])
+        self.presses += 1
 
-    def print_button_state(self, checked):
-        print("Button is:", "pressed" if checked else "not pressed")
+    def window_title_changed(self):
+        if self.presses == len(window_titles) - 1:
+            self.button.setDisabled(True)
+            self.button_2.setDisabled(True)
+            self.button.setText("Broken")
+            self.button_2.setText("Smashed")
 
 app = QApplication([])
 
