32a43dab9a71ea05bf37de3e7d2fd54123fef90d
WilfredSalmon <was29@cam.ac.uk>
2024-08-01 20:04:36 +0100

    basic events

--- third_application.py
+++ third_application.py
@@ -1,5 +1,6 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget
-from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget, QMenu
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtGui import QAction
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -9,6 +10,7 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(QSize(1000, 400))
 
         self.label = QLabel()
+        self.mouse_label = QLabel()
 
         self.input = QLineEdit()
         self.input.textChanged.connect(self.label.setText)
@@ -16,11 +18,47 @@ class MainWindow(QMainWindow):
         layout = QVBoxLayout()
         layout.addWidget(self.input)
         layout.addWidget(self.label)
+        layout.addWidget(self.mouse_label)
 
         container = QWidget()
         container.setLayout(layout)
 
         self.setCentralWidget(container)
+
+    def mouseMoveEvent(self, e):
+        self.mouse_label.setText("Mouse Moved!")
+
+    def mousePressEvent(self, e):
+        if e.button() == Qt.MouseButton.LeftButton:
+            print("left button")
+        elif e.button() == Qt.MouseButton.RightButton:
+            print("right button")
+        elif e.button() == Qt.MouseButton.MiddleButton:
+            print("middle button")
+
+        self.mouse_label.setText("Mouse pressed!")
+    
+    def mouseReleaseEvent(self, e):
+        self.mouse_label.setText("Mouse released!")
+    
+    def mouseDoubleClickEvent(self, e):
+        self.mouse_label.setText("Mouse double-clicked!")
+
+    def contextMenuEvent(self, e):
+        context = QMenu(self)
+        context.addAction(MenuLogger("option 1", self))
+        context.addAction(MenuLogger("option 2", self))
+        context.addAction(MenuLogger("option 3", self))
+        context.exec(e.globalPos())
+
+class MenuLogger(QAction):
+    def __init__(self, name, parent):
+        super().__init__(name, parent)
+        self.name = name
+        self.triggered.connect(self.log_name)
+
+    def log_name(self):
+        print(self.name)
 
 app = QApplication([])
 
